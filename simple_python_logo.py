26b8eecb4306d53cad6ca5658e12a38f324cf765
dolsbrandon <59302047+dolsbrandon@users.noreply.github.com>
2019-12-28 15:47:14 -0600

    Add files via upload

---
+++ simple_python_logo.py
@@ -0,0 +1,11 @@
+#Python Logo created by Brandon Dols
+
+print('          _______          ')
+print('         / o      \        ')
+print('     ___|_____     |___    ')
+print('    /              |   \   ')
+print('   /      ________/     \  ')
+print('   \     /              /  ')
+print('    \___/      ________/   ')
+print('        |       o |        ')
+print('         \_______/         ')
